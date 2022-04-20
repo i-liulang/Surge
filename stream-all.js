2183b6c5014dcbd36af173337033bcf3a07271de
i-liulang <75568294+i-liulang@users.noreply.github.com>
2022-04-20 12:43:36 +0800

    Update stream-all.js

--- stream-all.js
+++ stream-all.js
@@ -23,7 +23,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       title: '流媒体解锁检测',
       content: '',
       icon: '4k.tv',
-      'icon-color': '#00cc33',
+      'icon-color': '##FF2D55',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])
