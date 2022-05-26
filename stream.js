c2ed02a0cdbcbd4f6cdbea3c6ff83421e33a8829
i-liulang <75568294+i-liulang@users.noreply.github.com>
2022-05-26 16:48:04 +0800

    Update stream.js

--- stream.js
+++ stream.js
@@ -273,7 +273,9 @@ panel_result['content'] = content
                 inSupportedLocation,
                 location: { countryCode },
               },
-            } = data?.extensions?.sdk  
+            } = data?.extensions?.sdk
+            //如果是台湾地区，则将TW转换为CN，避免国行手机获取不到emoj
+            countryCode=(countryCode=="TW")?"CN":countryCode
             resolve({ inSupportedLocation, countryCode, accessToken })
           })
         })
