3ff56bed64ccf9d486795a54d459cfedcdbe6781
i-liulang <75568294+i-liulang@users.noreply.github.com>
2022-06-24 22:15:41 +0800

    Update stream.js

--- stream.js
+++ stream.js
@@ -51,17 +51,17 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
  let disney_result=""
     if (status==STATUS_COMING) {
         //console.log(1)
-        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆➕: 即将登陆~"+region.toUpperCase()
+        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆✛: 即将登陆~"+region.toUpperCase()
       } else if (status==STATUS_AVAILABLE){
         //console.log(2)
         console.log(region)
-        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆➕: 已经解锁 ➠ " +`${getFlagEmoji(region)} ` + region.toUpperCase()
+        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆✛: 已经解锁 ➠ " +`${getFlagEmoji(region)} ` + region.toUpperCase()
         // console.log(result["Disney"])
       } else if (status==STATUS_NOT_AVAILABLE) {
         //console.log(3)
-        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆➕: 没有支持 🚫 "
+        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆✛: 没有支持 🚫 "
       } else if (status==STATUS_TIMEOUT) {
-        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆➕: 检测超时 🚦"
+        disney_result="𝗗𝗶𝘀𝗻𝗲𝘆✛: 检测超时 🚦"
       }
 result.push(disney_result)
 console.log(result)
