d5c89d4b9d031307974da98470e181623408ba65
i-liulang <75568294+i-liulang@users.noreply.github.com>
2023-02-20 11:13:24 +0800

    Update ConnectivityTest.js

--- ConnectivityTest.js
+++ ConnectivityTest.js
@@ -1,13 +1,14 @@
 // 加入了百度和bilibili测速,感谢@小白脸重写脚本原脚本,原作者@yibeizipeini来自于https://raw.githubusercontent.com/yibeizipeini/JavaScript/Surge/ConnectivityTest.js
 let $ = {
-BL:'https://www.bilibili.com',
-BD:'https://www.baidu.com',
-YT:'https://www.youtube.com/',
-GG:'https://www.google.com/generate_204',
-GB:'https://www.github.com'
+Bilibili:'https://www.bilibili.com',
+Baidu:'https://www.baidu.com',
+Youtube:'https://www.youtube.com/',
+Google:'https://www.google.com/generate_204',
+Github:'https://www.github.com'
 }
+
 !(async () => {
-await Promise.all([http('BL'),http('BD'),http('GB'),http('GG'),http('YT')]).then((x)=>{
+await Promise.all([http('Baidu'),http('Bilibili'),http('Github'),http('Google'),http('Youtube')]).then((x)=>{
 	$done({
     title: 'Network Connectivity Test',
     content: x.join(' | '),
