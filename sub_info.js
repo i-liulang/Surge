889846d74522996f09c8d69084912183c334dc27
i-liulang <75568294+i-liulang@users.noreply.github.com>
2023-02-20 13:16:16 +0800

    Update sub_info.js

--- sub_info.js
+++ sub_info.js
@@ -58,7 +58,7 @@ Sub_info = script-name=Sub_info,update-interval=86400
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} ` + `| 𝗥𝗲𝘀𝗲𝘁 : ` + `${resetDayLeft} Days`,
+    title: `${args.title} ` + `| 𝗥𝗘𝗦𝗘𝗧 : ` + `${resetDayLeft} Days`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
