479628e7ccab960083415b0585ef318b7f8a2b96
i-liulang <75568294+i-liulang@users.noreply.github.com>
2023-02-20 11:25:55 +0800

    Update ConnectivityTest.js

--- ConnectivityTest.js
+++ ConnectivityTest.js
@@ -1,16 +1,16 @@
 let $ = {
-Bilibili:'https://www.bilibili.com',
-Baidu:'https://www.baidu.com',
-Youtube:'https://www.youtube.com/',
-Google:'https://www.google.com/generate_204',
-Github:'https://www.github.com'
+BL:'https://www.bilibili.com',
+BD:'https://www.baidu.com',
+YT:'https://www.youtube.com/',
+GG:'https://www.google.com/generate_204',
+GB:'https://www.github.com'
 }
 
 !(async () => {
-await Promise.all([http('Baidu'),http('Bilibili'),http('Github'),http('Google'),http('Youtube')]).then((x)=>{
+await Promise.all([http('BD'),http('BL'),http('GB'),http('GG'),http('YT')]).then((x)=>{
 	$done({
     title: 'Network Connectivity Test',
-    content: x.join('\n'),
+    content: x.join('|'),
     icon: 'timer',
     'icon-color': '#FF5A9AF9',
   })
